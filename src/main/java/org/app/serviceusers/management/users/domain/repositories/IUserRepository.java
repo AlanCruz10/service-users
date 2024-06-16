3a717060c8f50ee51d78180149a2d95f03a97f99
Alan George Trinidad Cruz <100612234+AlanCruz10@users.noreply.github.com>
2024-06-16 15:02:44 -0600

    [ADD] domain and reneame directory users.management to management.users (#1)
    Co-authored-by: AlanCruz10 <exalan30@gmail.com>

---
+++ src/main/java/org/app/serviceusers/management/users/domain/repositories/IUserRepository.java
@@ -0,0 +1,6 @@
+package org.app.serviceusers.management.users.domain.repositories;
+
+import org.app.serviceusers.management.users.domain.models.User;
+
+public interface IUserRepository extends BaseRepository<User>{
+}
